688e3169a64f3f54b40c74ef68118eb3c59d6d3d
dkf <dkf>
2021-03-27 10:25:31 +0000

    More tinkering with JS

--- scripts/toggles.js
+++ scripts/toggles.js
@@ -1,12 +1,8 @@
-// This implements the filtering toggle boxes used on the main index page.
+// This file implements the filtering toggle boxes used on the main index page.
 
-var sheet = (function(){
-    for (var i=0; i<document.styleSheets.length; i++) {
-        var s = document.styleSheets[i];
-        if (s.title == "filtering") {
-            return s;
-        }
-    }
+var sheet = (function() {
+    var s = document.getElementById("facetFilteringSheet");
+    return s.sheet ? s.sheet : s.styleSheet;
 })();
 
 function isChecked(boxes) {
@@ -22,8 +18,8 @@ function clearFacetClasses(facet) {
     var r = sheet.cssRules;
     // Must go through the rules backwards to keep indices consistent
     for (var i = r.length; i--; ) {
-        if (r[i].selectorText &&
-                r[i].selectorText.substr(1, facet.length).toLowerCase() === facet) {
+        var sel = r[i].selectorText;
+        if (sel && sel.substr(1, facet.length).toLowerCase() === facet) {
             sheet.deleteRule(i);
         }
     }
@@ -31,14 +27,14 @@ function clearFacetClasses(facet) {
 
 function addFacetClasses(boxes, facet) {
     for (var i = 0; i < boxes.length; i++) {
-	var ruleClass = facet + "-" + boxes[i].dataset.value;
+        var ruleClass = facet + "-" + boxes[i].dataset.value;
         if (boxes[i].checked) {
             // sheet.insertRule("."+ruleClass + " {display:table-row;}", 0);
-	} else {
+        } else {
             sheet.insertRule("." + ruleClass + " {display:none;}", 0);
         }
     }
-}
+};
 
 function toggleFacet(facet) {
     if (!sheet.cssRules) {
@@ -48,16 +44,16 @@ function toggleFacet(facet) {
     var none = isChecked(boxes);
     clearFacetClasses(facet);
     if (!none) {
-	addFacetClasses(boxes, facet);
+        addFacetClasses(boxes, facet);
     }
-}
+};
 
 function installToggleHandler(facet) {
     var boxes = document.getElementsByClassName("toggler-" + facet);
     for (var i = 0; i < boxes.length; i++) {
         boxes[i].onclick = (function() { toggleFacet(facet); });
     }
-}
+};
 
 window.onload = function() {
     installToggleHandler("state");
